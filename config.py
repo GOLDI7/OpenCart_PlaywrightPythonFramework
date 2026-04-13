b1e0cce47ea26a9eda5a67ace39c9c9e81312bfc
Aayaan Alam <aayanazam9211@gmail.com>
2026-04-13 15:26:54 +0530

    First Commit

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    email="aayaan@outlook.com"
+    password="sHahara#123"
+
+    invalid_email="aayan123@abc.com"
+    invalid_password="test@123xyz"
+    product_name = "MacBook"
+    product_quantity = "1"
+    total_price = "$602.00"
+    
